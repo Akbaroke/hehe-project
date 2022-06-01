e40bd5c9b58f188fe013e41392f47dd2d2f26135
Fajar Nugroho <52228231+fajaragngn@users.noreply.github.com>
2022-06-01 23:50:05 +0700

    rebuilding site Wed Jun  1 23:50:05 WIB 2022

--- customer/customer.php
+++ customer/customer.php
@@ -57,62 +57,49 @@ if ($file == "checkout.php") {
 </div>
 <!-- /BREADCRUMB -->
 
-<div class="section">
-	<div class="container">
-		<div class="row">
+
 			
 			<?php 
 			include 'customer_sidebar.php'; 
 			?>
 
-			<div id="main" class="col-md-9">
-				
-				<h4>DASHBOARD</h4>
+<div id="main" class="col-md-9">
 
-				<div id="store">
+<h4>DASHBOARD</h4>
+	<h5>Halo, Selamat Datang!</h5>
 
-					<div class="row">
-
-						<div class="col-lg-12">
-							
-							<h5>Halo, Selamat Datang!</h5>
-
-							<table class="table table-bordered">
-								<tbody>
-									<?php 
-									$id = $_SESSION['customer_id'];
-									$customer = mysqli_query($koneksi,"select * from customer where customer_id='$id'");
-									while($i = mysqli_fetch_array($customer)){
-										?>
-										<tr>
-											<th width="20%">Nama</th>	
-											<td><?php echo $i['customer_nama'] ?></td>
-										</tr>
-										<tr>
-											<th width="20%">Email</th>	
-											<td><?php echo $i['customer_email'] ?></td>
-										</tr>
-										<tr>
-											<th>HP</th>	
-											<td><?php echo $i['customer_hp'] ?></td>
-										</tr>
-										<tr>
-											<th>Alamat</th>	
-											<td><?php echo $i['customer_alamat'] ?></td>
-										</tr>
-										<?php 
-									}
-									?>
-								</tbody>
-							</table>
-						</div>
+	<table class="table table-bordered">
+		<tbody>
+			<?php 
+			$id = $_SESSION['customer_id'];
+			$customer = mysqli_query($koneksi,"select * from customer where customer_id='$id'");
+			while($i = mysqli_fetch_array($customer)){
+				?>
+				<tr>
+					<th width="20%">Nama</th>	
+					<td><?php echo $i['customer_nama'] ?></td>
+				</tr>
+				<tr>
+					<th width="20%">Email</th>	
+					<td><?php echo $i['customer_email'] ?></td>
+				</tr>
+				<tr>
+					<th>HP</th>	
+					<td><?php echo $i['customer_hp'] ?></td>
+				</tr>
+				<tr>
+					<th>Alamat</th>	
+					<td><?php echo $i['customer_alamat'] ?></td>
+				</tr>
+				<?php 
+			}
+			?>
+		</tbody>
+	</table>
 
 
-					</div>
-				</div>
 
-			</div>
-		</div>
 	</div>
-</div>
 
+</body>
+</html>
