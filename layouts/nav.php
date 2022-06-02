3a6670f7e455b622903b2dcf17c50dffcae0220f
akbaroke <akbarmuhammad833@gmail.com>
2022-06-02 17:45:07 +0700

    rebuilding site Thu, Jun  2, 2022  5:45:07 PM

--- layouts/nav.php
+++ layouts/nav.php
@@ -16,17 +16,19 @@
             <div class="con-ham"><div class="hamburger"><i class="fa-solid fa-bars"></i></div></div>
         </nav>
         <div class="nav-mobile">
-            <div class="con-close"><div class="close"><i class="fas fa-times"></i></div></div>
-            <div class="search-container-mobile">
-                <form action="" method="get">
-                    <input type="text" name="keyword" class="kol-search" placeholder="Cari...">
-                    <button type="submit" name="search"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </form>
-            </div>
-            <div class="tom-container-mobile">
-                <a href="#"><div class="tom"><i class="fa-solid fa-book"></i></div></a>
-                <a href="#"><div class="tom"><i class="fa-solid fa-basket-shopping"></i></div></a>
-                <a href="login.php"><div class="tom"><i class="fa-solid fa-user"></i></div></a>
+            <div class="nav-mobile-container">
+                <div class="tom-container-mobile">
+                    <a href="login.php"><div class="tom"><i class="fa-solid fa-user"></i></div></a>
+                    <a href="#"><div class="tom"><i class="fa-solid fa-basket-shopping"></i></div></a>
+                    <a href="#"><div class="tom"><i class="fa-solid fa-book"></i></div></a>
+                </div>
+                <div class="con-close"><div class="close"><i class="fas fa-times"></i></div></div>
+                <div class="search-container-mobile">
+                    <form action="" method="get">
+                        <input type="text" name="keyword" class="kol-search" placeholder="Cari...">
+                        <button type="submit" name="search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
