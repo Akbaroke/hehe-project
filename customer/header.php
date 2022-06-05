eaaf4fd58339ad84b1ab9f125f9e3d63fabe99b2
Fajar Nugroho <52228231+fajaragngn@users.noreply.github.com>
2022-06-05 18:33:15 +0700

    rebuilding site Sun Jun  5 18:33:15 WIB 2022

--- customer/header.php
+++ customer/header.php
@@ -24,7 +24,7 @@
 
     <section id="nav">
         <nav class="nav">
-            <div class="logo"><a href="index.php"><img src="../assets/img/landing/LOGO.png" alt="LOGO"></a></div>
+            <div class="logo"><a href="../index"><img src="../assets/img/landing/LOGO.png" alt="LOGO"></a></div>
             <div class="search-container">
                 <form action="" method="get">
                     <input type="text" name="keyword" class="kol-search" placeholder="Cari...">
@@ -33,7 +33,7 @@
             </div>
             <div class="tom-container">
                 <a href="#"><div class="tom"><i class="fa-solid fa-book"></i></div></a>
-                <a href="#"><div class="tom"><i class="fa-solid fa-basket-shopping"></i></div></a>
+                <a href="../keranjang/"><div class="tom"><i class="fa-solid fa-shopping-cart"></i></div></a>
                 <a href="../login"><div class="tom"><i class="fa-solid fa-user"></i></div></a>
             </div>
             <div class="con-ham"><div class="hamburger"><i class="fa-solid fa-bars"></i></div></div>
@@ -42,8 +42,8 @@
             <div class="nav-mobile-container">
                 <div class="tom-container-mobile">
                     <a href="login.php"><div class="tom"><i class="fa-solid fa-user"></i></div></a>
-                    <a href="#"><div class="tom"><i class="fa-solid fa-basket-shopping"></i></div></a>
-                    <a href="#"><div class="tom"><i class="fa-solid fa-book"></i></div></a>
+                    <a href="../keranjang/"><div class="tom"><i class="fa-solid fa-shopping-cart"></i></div></a>
+                    <a href="../login"><div class="tom"><i class="fa-solid fa-book"></i></div></a>
                 </div>
                 <div class="con-close"><div class="close"><i class="fas fa-times"></i></div></div>
                 <div class="search-container-mobile">
