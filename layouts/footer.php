4578a778f28b79d887491cc9f41bb47499b2eb30
Fajar Nugroho <52228231+fajaragngn@users.noreply.github.com>
2022-06-06 23:15:11 +0700

    rebuilding site Mon Jun  6 23:15:11 WIB 2022

--- layouts/footer.php
+++ layouts/footer.php
@@ -23,18 +23,24 @@
                 </div>
                 <div class="box-footer">
                     <h2>Ikuti Kami</h2>
-                    <div class="ikut-box">
+                    <!--div class="ikut-box">
                         <div><a href="#"><i class="fa-brands fa-facebook-f"></i></a></div>
                         <div><a href="#"><i class="fa-brands fa-twitter"></i></a></div>
                         <div><a href="#"><i class="fa-brands fa-instagram"></i></a></div>
                         <div><a href="#"><i class="fa-brands fa-whatsapp"></i></a></div>
-                    </div>
+                    </div-->
+                    <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
                 </div>
             </div>
         </div>
     </section>
     <footer>
-        <div><h1>@2022 Sandio PetCare</h1></div>
+        <div><h1>@<script>document.write(new Date().getFullYear());</script> Sandio PetCare</h1></div>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="assets/js/produk_detail.js"></script>
